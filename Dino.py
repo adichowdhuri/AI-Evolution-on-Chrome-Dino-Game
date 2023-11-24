3d4e3464a176abbb1c0baa5fbfe2a089b44c41e6
adichowdhuri <adichowdhuri@gmail.com>
2023-11-24 18:42:29 +0530

    added ability to duck

--- Dino.py
+++ Dino.py
@@ -1,98 +1,179 @@
 import pygame
-
-from DinoAI import SCREEN_WIDTH, Dinosaur
+import random
 
 pygame.init()
 
-#GLOBAL CONSTS
-SCREEN_HEIGHTS = 600
+# Global Constants
+SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHTS))
+points = 0
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-RUNNING = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoRun1.png"), 
-            pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoRun2.png")]
+# Load images
+def load_image(image_path):
+    return pygame.image.load(image_path)
 
-JUMPING = pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoJump.png")
-DUCKING = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoDuck1.png"), 
-            pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoDuck2.png")]
+RUNNING = [load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoRun1.png"),
+           load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoRun2.png")]
 
-SMALL_CACTUS = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\SmallCactus1.png"),
-                pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\SmallCactus2.png"),
-                pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\SmallCactus3.png")]
+DUCKING = [load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoDuck1.png"),
+           load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoDuck2.png")]
 
-LARGE_CACTUS = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\LargeCactus1.png"),
-                pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\LargeCactus2.png"),
-                pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\LargeCactus3.png")]
+JUMPING = load_image(r'AI-Evolution-on-Chrome-Dino-Game\images\DinoJump.png')
 
-BIRD = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\Bird1.png"), 
-            pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\Bird2.png")]
+SMALL_CACTUS = [load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\SmallCactus1.png"),
+                load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\SmallCactus2.png"),
+                load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\SmallCactus3.png")]
 
-CLOUD = pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\Cloud.png")
+LARGE_CACTUS = [load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\LargeCactus1.png"),
+                load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\LargeCactus2.png"),
+                load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\LargeCactus3.png")]
 
-BG = pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\Track.png")
+BIRD = [load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\Bird1.png"),
+        load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\Bird2.png")]
+
+CLOUD = load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\Cloud.png")
+BG = load_image(r"AI-Evolution-on-Chrome-Dino-Game\images\Track.png")
+
+FONT = pygame.font.Font('freesansbold.ttf', 20)
 
 class Dinosaur:
     X_POS = 80
     Y_POS = 310
+    JUMP_VEL = 8.5
 
-    def __init__(self):
-        self.duck_img = DUCKING
-        self.run_img = RUNNING
-        self.jump_img = JUMPING
-
-        self.dino_duck = False
-        self.dino_jump = False
+    def __init__(self, img=RUNNING[0]):
+        self.image = img
         self.dino_run = True
-
+        self.dino_jump = False
+        self.dino_duck = False
+        self.jump_vel = self.JUMP_VEL
+        self.rect = pygame.Rect(self.X_POS, self.Y_POS, img.get_width(), img.get_height())
         self.step_index = 0
-        self.img = self.run_img[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = self.X_POS
-        self.dino_rect.y = self.Y_POS
-    
+
     def update(self, user_input):
-        if self.dino_duck:
-            self.duck()
-        if self.dimo_run:
+        if self.dino_run:
             self.run()
         if self.dino_jump:
             self.jump()
+        if self.dino_duck:
+            self.duck()
 
         if self.step_index >= 10:
             self.step_index = 0
 
-        if user_input[pygame.K_SPACE] and not self.dino_jump:
+        if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
-            self.dino_duck = False
             self.dino_run = False
+            self.dino_duck = False
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_jump = False
-            self.dino_duck = True
             self.dino_run = False
-        elif not(self.dino_duck or self.dino_jump):
+            self.dino_duck = True
+        elif not (self.dino_duck or self.dino_jump):
+            self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
 
-        def run(self):
-            self.image = self.run_img[self.step_index // 5]
-            self.dino_rect = self.image.get_rect()
-            self.dino_rect.x = self.X_POS
-            self.dino_rect.y = self.Y_POS
-            self.step_index += 1
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+        if self.jump_vel <= -self.JUMP_VEL:
+            self.dino_jump = False
+            self.jump_vel = self.JUMP_VEL
+            self.rect.y = self.Y_POS
+
+    def duck(self):
+        self.image = DUCKING[self.step_index // 5]
+        self.rect.y = self.Y_POS + 32
+        self.step_index += 1
+
+    def run(self):
+        self.image = RUNNING[self.step_index // 5]
+        self.rect.y = self.Y_POS
+        self.step_index += 1
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+
+class Obstacle:
+    def __init__(self, image, number_of_cacti):
+        self.image = image
+        self.type = number_of_cacti
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update(self):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
+
+class SmallCactus(Obstacle):
+    def __init__(self, image, number_of_cacti):
+        super().__init__(image, number_of_cacti)
+        self.rect.y = 325
+
+class LargeCactus(Obstacle):
+    def __init__(self, image, number_of_cacti):
+        super().__init__(image, number_of_cacti)
+        self.rect.y = 300
+
+class Bird(Obstacle):
+    def __init__(self, image, number_of_cacti):
+        super().__init__(image, number_of_cacti)
+        self.rect.y = 250 if number_of_cacti % 2 == 0 else 300  # Randomize bird height
 
 def main():
+    global game_speed, obstacles
     run = True
     clock = pygame.time.Clock()
     player = Dinosaur()
+    game_speed = 20
+    obstacles = []
+
+    def score():
+        global points, game_speed
+        points += 1
+        if points % 100 == 0:
+            game_speed += 1
+        text = FONT.render(f'Points:  {str(points)}', True, (0, 0, 0))
+        SCREEN.blit(text, (950, 50))
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
+                run = False
 
         SCREEN.fill((255,255,255))
-        user_input = pygame.key.get_pressed()
 
-        player.draw(SCREEN)
+        for obstacle in obstacles:
+            obstacle.update()
+            obstacle.draw(SCREEN)
+            if player.rect.colliderect(obstacle.rect):
+                run = False
+
+        if len(obstacles) == 0 or obstacles[-1].rect.x < 500:
+            if random.randint(0, 2) == 0:
+                obstacles.append(SmallCactus(SMALL_CACTUS, random.randint(0, 2)))
+            elif random.randint(0, 2) == 1:
+                obstacles.append(LargeCactus(LARGE_CACTUS, random.randint(0, 2)))
+            else:
+                obstacles.append(Bird(BIRD, random.randint(0, 1)))
+
+        user_input = pygame.event.get()
         player.update(user_input)
+        player.draw(SCREEN)
+
+        score()
+        pygame.display.update()
         clock.tick(30)
 
-main()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
--- DinoAI.py
+++ DinoAI.py
@@ -16,6 +16,9 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 RUNNING = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoRun1.png"),
            pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoRun2.png") ]
 
+DUCKING = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoDuck1.png"), 
+            pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\DinoDuck2.png")]
+
 JUMPING = pygame.image.load(r'AI-Evolution-on-Chrome-Dino-Game\images\DinoJump.png')
 
 SMALL_CACTUS = [pygame.image.load(r"AI-Evolution-on-Chrome-Dino-Game\images\SmallCactus1.png"),
@@ -44,6 +47,7 @@ class Dinosaur:
         self.image = img
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
         self.rect = pygame.Rect(self.X_POS, self.Y_POS, img.get_width(), img.get_height())
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
@@ -54,6 +58,8 @@ class Dinosaur:
             self.run()
         if self.dino_jump:
             self.jump()
+        if self.dino_duck:
+            self.duck()
         if self.step_index >= 10:
             self.step_index = 0
 
@@ -66,6 +72,15 @@ class Dinosaur:
             self.dino_jump = False
             self.dino_run = True
             self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[self.step_index // 5]
+        self.rect.x = self.X_POS 
+        self.rect.y = self.Y_POS + 32
+        self.step_index += 1
+    
+    def release_duck(self):
+        self.dino_duck = False
     
     def run(self):
         self.image = RUNNING[self.step_index // 5]
@@ -87,15 +102,25 @@ class Obstacle:
         self.rect = self.image[self.type].get_rect()
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.rect.x = SCREEN_WIDTH
+        self.step_index = 0
+
     
     def update(self):
         self.rect.x -= game_speed
+        self.step_index += 1
+        if self.step_index >= 10:
+            self.step_index = 0
         if self.rect.x < -self.rect.width:
             obstacles.pop()
 
     def draw(self, SCREEN):
-        SCREEN.blit(self.image[self.type], self.rect)
-        pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 2)
+        if not isinstance(self, Bird):
+            SCREEN.blit(self.image[self.type], self.rect)
+            pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 2)
+        else:       
+            self.image = BIRD[self.step_index // 5]
+            SCREEN.blit(self.image, self.rect)
+            pygame.draw.rect(SCREEN, self.color, (self.rect.x, self.rect.y, self.rect.width, self.rect.height), 2)
 
 class SmallCactus(Obstacle):
     def __init__(self, image, number_of_cacti):
@@ -107,14 +132,30 @@ class LargeCactus(Obstacle):
         super().__init__(image, number_of_cacti)
         self.rect.y = 300
 
+class Bird(Obstacle):
+    def __init__(self, image, number_of_cacti):
+        super().__init__(image, 1)
+        self.rect.y = 270
+
+class HighBird(Bird):
+    def __init__(self, image):
+        super().__init__(image, 0)
+        self.rect.y = 220  # Higher position
+
+class LowBird(Bird):
+    def __init__(self, image):
+        super().__init__(image, 0)
+        self.rect.y = 300  # Lower position
+
+
 def remove(index):
     dinosaurs.pop(index)
     ge.pop(index)
     nets.pop(index)
 
 def distance(pos_a, pos_b):
-    dx = pos_a[0]-pos_b[0]
-    dy = pos_a[1]-pos_b[1]
+    dx = pos_a-pos_b
+    dy = pos_a-pos_b
     return math.sqrt(dx**2+dy**2)
 
 def eval_genomes(genomes, config):
@@ -140,7 +181,7 @@ def eval_genomes(genomes, config):
 
     def score():
         global points, game_speed
-        points += 1
+        points += 5
         if points % 100 == 0:
             game_speed += 1
         text = FONT.render(f'Points:  {str(points)}', True, (0, 0, 0))
@@ -184,11 +225,19 @@ def eval_genomes(genomes, config):
            break
 
         if len(obstacles) == 0:
-            rand_int = random.randint(0, 1)
+            # if points > 1000:
+            #     rand_int = random.randint(0, 3)
+            # else:
+            #     rand_int = random.randint(0, 1)
+            rand_int = random.randint(0, 3)
             if rand_int == 0:
                 obstacles.append(SmallCactus(SMALL_CACTUS, random.randint(0, 2)))
             elif rand_int == 1:
                 obstacles.append(LargeCactus(LARGE_CACTUS, random.randint(0, 2)))
+            elif rand_int == 2:
+                obstacles.append(LowBird(BIRD))
+            elif rand_int == 3:
+                obstacles.append(HighBird(BIRD))
 
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
@@ -202,11 +251,29 @@ def eval_genomes(genomes, config):
 
         for i, dinosaur in enumerate(dinosaurs):
             output = nets[i].activate((dinosaur.rect.y,
-                                       distance((dinosaur.rect.x, dinosaur.rect.y),
-                                        obstacle.rect.midtop)))
+                                       distance(dinosaur.rect.x, obstacle.rect.x),
+                                       distance(dinosaur.rect.y, obstacle.rect.y)
+                                    ))
+
+            # Decision to jump
             if output[0] > 0.5 and dinosaur.rect.y == dinosaur.Y_POS:
                 dinosaur.dino_jump = True
-                dinosaur.dino_run = False        
+                dinosaur.dino_run = False
+                dinosaur.dino_duck = False
+
+            # Decision to duck
+            elif output[1] > 0.5 and dinosaur.rect.y == dinosaur.Y_POS:
+                dinosaur.dino_jump = False
+                dinosaur.dino_run = False
+                dinosaur.dino_duck = True
+
+            # Decision to stop ducking
+            elif output[2] > 0.5 and dinosaur.rect.y == (dinosaur.Y_POS + 32):
+                dinosaur.release_duck()
+                dinosaur.dino_jump = False
+                dinosaur.dino_run = True
+
+
 
         statistics()
         score()
